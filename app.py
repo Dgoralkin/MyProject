1ee5ebc747bb222a28611e221609ab71246da499
Dany Goralkin <Goralkin@Gmail.com>
2022-07-17 23:05:02 +0300

    temp created

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
-
 from helpers import apology, login_required
+
+# Heroku app location: https://final-project-dany.herokuapp.com/
 
 # Configure application
 app = Flask(__name__)
