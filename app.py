8c2de09a6206d1312a1e37b13eecc8fca69bbe13
Dany Goralkin <Goralkin@Gmail.com>
2022-07-21 23:42:00 +0300

    new DB connection established

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ Session(app)
 # Configure MySql connection to DataBase For app Manager
 db = mysql.connector.connect(
     host="eu-cdbr-west-03.cleardb.net",
-    user='ba430e02e4e6b8',
-    passwd='1fbec195',
-    database="heroku_c982995c47a34c9"
+    user='b9886f5b189f60',
+    passwd='0190bc04',
+    database="heroku_3003039de5cde26"
 )
 
 if (db):
