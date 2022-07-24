5bb06220008ef66f7a26f160193b6ffae9e14352
Dany Goralkin <Goralkin@Gmail.com>
2022-07-24 18:59:24 +0300

    New heroko detail2

--- app.py
+++ app.py
@@ -29,7 +29,8 @@ db = mysql.connector.connect(
     host="eu-cdbr-west-03.cleardb.net",
     user='b62d0c2852c752',
     passwd='047bddc0',
-    database="heroku_666bfee5e0eaef3"
+    database="heroku_666bfee5e0eaef3",
+    reconnect=True
 )
 
 if (db):
